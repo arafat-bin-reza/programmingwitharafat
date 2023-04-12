ca42b6ee8d1e62b84999cc10d57d97af3746686f
arafat-bin-reza <arafat.reza.developer@gmail.com>
2023-04-13 00:21:23 +0600

    Variables and Constants project added.

---
+++ Learning CSharp with Arafat/Learn C# Fundamentals (Basics for Beginners)/Console/Variables_and_Constants/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Variables_and_Constants
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'A';
+            string firstName = "Arafat";
+            bool isWorking = false;
+            var number2 = 22;
+            const float PI = 3.14f;
+            //PI = 1;
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(number2);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+        }
+    }
+}
