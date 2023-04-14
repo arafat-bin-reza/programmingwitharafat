4a58a8107af2ed873dd68f972e41830bc5a35461
arafat-bin-reza <arafat.reza.developer@gmail.com>
2023-04-15 00:14:08 +0600

    Operators project added.

---
+++ Learning CSharp with Arafat/Learn C# Fundamentals (Basics for Beginners)/Console/Operators/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 100;
+            var b = 20;
+            var c = 30;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+
+            Console.WriteLine(a / c);
+            Console.WriteLine(a % c);
+
+            Console.WriteLine((float)a / (float)c);
+
+            Console.WriteLine(a + b * c);
+            Console.WriteLine((a + b) * c);
+
+            Console.WriteLine(a > b);
+            Console.WriteLine(a == b);
+            Console.WriteLine(a != b);
+            Console.WriteLine(!(a != b));
+
+            Console.WriteLine(c > a && c > b);
+            Console.WriteLine(c > a && c == b);
+
+            Console.WriteLine(c > a || c == b);
+            Console.WriteLine(!(c > a || c == b));
+
+        }
+    }
+}
