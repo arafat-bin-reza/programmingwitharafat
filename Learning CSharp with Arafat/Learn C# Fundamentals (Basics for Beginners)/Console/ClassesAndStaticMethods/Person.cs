f5fae280f70d6c22790cd57d573fa8bf2bf7ed85
arafat-bin-reza <arafat.reza.developer@gmail.com>
2023-04-15 01:11:50 +0600

    ClassesAndStaticMethods project added.

---
+++ Learning CSharp with Arafat/Learn C# Fundamentals (Basics for Beginners)/Console/ClassesAndStaticMethods/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassesAndStaticMethods
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Introduction()
+        {
+            Console.WriteLine("My Name is {0} and I am {1} years old.", Name, Age);
+        }
+    }
+}
---
+++ Learning CSharp with Arafat/Learn C# Fundamentals (Basics for Beginners)/Console/ClassesAndStaticMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ClassesAndStaticMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Arafat = new Person();
+            Arafat.Name = "Arafat Bin Reza";
+            Arafat.Age = 25;
+            Arafat.Introduction();
+
+            var result = Calculator.Add(1,2); // We do not need to create an object of a class to call static methods.
+            System.Console.WriteLine(result);
+        }
+    }
+}
